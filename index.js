347993614128ce2130bc6ab94bef994160da8be7
Miliya Immatty Lazar <miliyaninu@gmail.com>
2024-10-22 13:49:55 +1100

    completed

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); // Convert the response to JSON
+  })
+  .then(books => {
+    renderBooks(books); // Pass the JSON data to renderBooks
+  })
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+  });
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
